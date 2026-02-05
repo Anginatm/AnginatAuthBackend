a53b49e0c29a7dc1bafef50dd6117c80b1f0ffb2
Taranjit Singh <tarankhalsa3412@gmail.com>
2026-02-05 19:05:07 +0530

    Add docker

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API routes
-app.use('/pr-auth/api', routes);
+app.use('/api', routes);
 
 // Root
 app.get('/', (req, res) => {
